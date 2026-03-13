c6c377be53c8019ea693368744d7d2aa865c07fd
Anita Dijala <dijalaanita@gmail.com>
2026-03-13 13:25:43 +0000

    created a navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import BrandAnalysis from "./pages/BrandAnalysis"
+import NavBar from "./components/NavBar"
 
 export default function App() {
   return (
+    <div>
+      <NavBar />
+    
     <div style={{ 
   padding: "40px", 
-  maxWidth: "1200px", 
-  margin: "0 auto", 
-  fontFamily: "'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
-  backgroundColor: "#f9f9f9",
-  minHeight: "100vh"
+  // maxWidth: "1200px", 
+  // margin: "0 auto", 
+  // fontFamily: "'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
+  // backgroundColor: "#f9f9f9",
+  // minHeight: "100vh"
 }}>
   <BrandAnalysis />
+</div>
 </div>
   )
 }
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+export default function NavBar(){
+
+    return(
+        <div style={{
+            padding: "15px",
+            background: "#111",
+            color: "white",
+            marginBottom: "30px"
+        }}>
+            <h2>LAGOS FASHION WEEK F/W 2025 ANALYSIS</h2>
+
+
+
+        </div>
+    )
+
+}
