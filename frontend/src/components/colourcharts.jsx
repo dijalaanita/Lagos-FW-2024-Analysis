220a27d95a9902ce0406f8b6bf56b9e2cc824d8d
Anita Dijala <dijalaanita@gmail.com>
2026-03-14 14:11:39 +0000

    attepmted to make the charts for the overview to show, didnt work

--- frontend/src/components/colourcharts.jsx
+++ frontend/src/components/colourcharts.jsx
@@ -1,6 +1,9 @@
 import {BarChart, Bar, XAxis, YAxis, Tooltip} from 'recharts'
 
 export default function ColourCharts({data}) {
+    console.log("recved", data)
+    if (!data || !Array.isArray(data)) return <div>Loading chart..</div>;
+     
     return (
         <BarChart width={600} height={300} data={data}>
             <XAxis dataKey="colour" />
--- frontend/src/pages/Overview.jsx
+++ frontend/src/pages/Overview.jsx
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react"
 import { getOverviewColours } from "../services/api"
-import ColourChart from "../components/ColourChart"
+import ColourCharts from "../components/colourcharts"
 import Insights from "../components/Insights"
 
 export default function Overview(){
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
 
-        getOverviewColours().then((response) => setData(response.data))
+        getOverviewColours()
+        .then((response) => {
+            setData(response.data);
+            setLoading(false);})
+        .catch((error) => {
+            console.error("failed to fetch colours", error);
+            setLoading(false);
+        });
 
-    }, [])
+    }, []);
 
+    if (loading) return <div>loading overall fashion trends...</div>
+    console.log("Overview Data:", data);
     return (
         <div>
             <h1>LAGOS FASHION WEEK COLOUR TRENDS</h1>
-            <ColourChart data={data}/>
+            <ColourCharts data={data}/>
             <Insights data={data}/>
             </div>
     )
